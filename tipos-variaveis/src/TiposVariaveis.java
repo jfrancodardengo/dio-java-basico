1837b2da72d5ce879dfe4d376caaa7b0ba09b306
jfrancodardengo <jfrancodardengo@gmail.com>
2024-10-18 15:39:40 -0300

    exemplos de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,18 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int idade;
+        int anoFabricacao = 2024;
+        double salario = 5000; /*aqui temos o valor de 5000,00. A representação de valor em milhares, é escrita sem as casas decimais*/
+        double salarioMinimo = 1700.50; /* aqui temos o valor de 1700,50. Usa-se as casas decimais representadas pelo ponto(.) */
+
+        final double VALOR_DE_PI = 3.14;
+        System.out.print("Valor de pi:" + VALOR_DE_PI);
+
+        /*aqui eu tenho um erro pois a variável é do tipo
+        constante e portanto não pode ter seu valor alterado */
+        // VALOR_DE_PI = 3.90; 
+
+        idade = 29;
+        System.out.print(idade);
     }
 }
